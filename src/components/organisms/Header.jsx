2dc0f0b9e062512a89d98c125b4aad9d3a5a84b0
gregorsunta <gresunta@gmail.com>
2023-02-23 09:09:06 +0100

    add react router and edit basic file structure to atomic design

---
+++ src/components/organisms/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <p>Reddot</p>
+      {/* search bar */}
+      {/* buttons? */}
+      {/* my profile button - sign out - sign out? */}
+    </div>
+  );
+};
+
+export default Header;
