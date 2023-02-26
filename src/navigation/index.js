6f004cb967f03a1af4c144c382d53eb6c99816df
gregorsunta <gresunta@gmail.com>
2023-02-26 10:08:08 +0100

    add local export file

---
+++ src/navigation/index.js
@@ -0,0 +1,3 @@
+export * from './PrivateRoute';
+export * from './ProtectedRoutes';
+export * from './RouterConfig';
---
+++ src/services/index.js
@@ -0,0 +1,4 @@
+export * from './firebase-auth';
+export * from './firebase-firestore';
+export * from './firebase-config';
+export * from './auth-observer';
