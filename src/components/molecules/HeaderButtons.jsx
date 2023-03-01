30a287cace57df7854d450a6864b8665f9371c4a
gregorsunta <gresunta@gmail.com>
2023-03-01 23:13:10 +0100

    modify returned component for unauthorized user

--- src/components/molecules/HeaderButtons.jsx
+++ src/components/molecules/HeaderButtons.jsx
@@ -1,10 +1,31 @@
 import { Button } from '../atoms';
+import { signInUser } from '../../services/firebase-auth';
+import styles from '../../styles/molecules/panels/HeaderButtons.module.css';
 
-const HeaderButtons = ({ auth }) => {
-  if (!auth.user) {
-    return <Button></Button>;
+const HeaderButtons = ({ auth = false }) => {
+  if (!auth) {
+    return (
+      <>
+        <Button
+          className={`${styles['text-button-container']}`}
+          title={'Click here to log in'}
+          text={'Log in'}
+          onClick={null}
+        />
+        <Button
+          className={`${styles['text-button-container']} ${styles['highlight']}`}
+          title={'Click here to sign up'}
+          text={'Sign up'}
+          onClick={null}
+        />
+      </>
+    );
   }
-  return;
+  return (
+    <>
+      <Button title />
+    </>
+  );
 };
 
-export default HeaderButtons;
+export { HeaderButtons };
