cc8754976cad7e6c348b25a55e930c00e3aaed73
gregorsunta <gresunta@gmail.com>
2023-03-01 23:16:34 +0100

    Add component and styling

---
+++ src/components/molecules/Dropdown.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useRef, useState } from 'react';
+import { Button } from '../atoms';
+import styles from '../../styles/molecules/Dropdown.module.css';
+import withRedirect from '../../utils/withRedirect.jsx';
+
+const ButtonWithRedirect = withRedirect(Button);
+
+const Dropdown = ({ options, className }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const container = useRef(null);
+
+  const toggleList = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    function handleClickOutside(e) {
+      if (container.current && !container.current.contains(e.target))
+        setIsOpen(false);
+    }
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [container]);
+  return (
+    <div className={`${styles.container} ${className}`} ref={container}>
+      <Button
+        className={`${isOpen && styles['active']}`}
+        text={'Profile'}
+        onClick={toggleList}
+      />
+      <ul className={`${isOpen && styles['active']}`}>
+        {options.map((option) => {
+          if (option.link) {
+            return (
+              <ButtonWithRedirect
+                // className={styles.option}
+                link={option.link}
+                text={option.name}
+                key={option.name}
+              >
+                {option.name}
+              </ButtonWithRedirect>
+            );
+          } else {
+            return (
+              <Button
+                onClick={option.onClick}
+                text={option.name}
+                key={option.name}
+              >
+                {option.name}
+              </Button>
+            );
+          }
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export { Dropdown };
