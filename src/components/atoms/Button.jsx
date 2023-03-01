9579dbc5654420f5aba258698f4ac88fc0d13443
gregorsunta <gresunta@gmail.com>
2023-03-01 23:10:31 +0100

    add component props

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,5 +1,16 @@
-const Button = ({ text }) => {
-  return <button>{text}</button>;
+import styles from '../../styles/atoms/Button.module.css';
+
+const Button = ({
+  text = null,
+  onClick = null,
+  className = '',
+  title = undefined,
+}) => {
+  return (
+    <button title={title} className={`${''} ${className}`} onClick={onClick}>
+      {text}
+    </button>
+  );
 };
 
 export { Button };
