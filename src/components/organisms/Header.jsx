e5f28a56909ab801b4d18c29ef5b51278eb7183a
gregorsunta <gresunta@gmail.com>
2023-03-04 10:35:15 +0100

    use extracted Logo component

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import logo from '../../assets/images/logo.svg';
 import styles from '../../styles/organisms/Header.module.css';
 import { HeaderButtons, Dropdown } from '../molecules/';
 import { Input } from '../atoms/';
+import withRedirect from '../../utils/withRedirect';
+import Logo from '../molecules/Logo.jsx';
 
 const options = [
   { name: 'Account', link: '/test1' },
   { name: 'Register or Sign Up', link: '/test1' },
 ];
 
+const RedirectLogo = withRedirect(Logo);
 const Header = ({ className }) => {
   return (
     <div className={`${styles.container} ${className}`}>
       <div className={styles['logo-container']}>
-        <img style={{ height: '80%' }} src={logo} alt="Outlined robot head." />
-        <p>reddot</p>
+        <RedirectLogo link={'/'} />
       </div>
-      {/* <div className={styles['search-group-container']}></div> */}
       <Input
         className={`${styles['search-bar-container']}`}
         placeholder={'Search reddot'}
