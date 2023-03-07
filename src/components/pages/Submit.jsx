d36ea328850249cdf679947cf079f2b6a97f95b0
gregorsunta <gresunta@gmail.com>
2023-03-07 16:12:46 +0100

    rename files

--- src/components/pages/Submit.jsx
+++
@@ -1,9 +0,0 @@
-import Protected from '../../navigation/Protected';
-import { SubmitPanel } from '../molecules/panels/';
-import MainTemplate from '../templates/MainTemplate.jsx';
-
-const Submit = () => {
-  return <MainTemplate content={<SubmitPanel />}></MainTemplate>;
-};
-
-export default Submit;
