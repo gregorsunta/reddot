06c9443ef73729e8db9397d10a662cbffb277da8
gregorsunta <gresunta@gmail.com>
2023-03-07 16:08:58 +0100

    remove file

--- src/services/auth-observer.js
+++
@@ -1,8 +0,0 @@
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-
-const auth = getAuth();
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-  } else {
-  }
-});
--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,3 @@
 export * from './firebase-auth';
 export * from './firebase-firestore';
 export * from './firebase-config';
-export * from './auth-observer';
