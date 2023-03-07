9da2da62b1b7c109e622ea9a28454437192487bb
gregorsunta <gresunta@gmail.com>
2023-03-07 22:12:15 +0100

    Rename and modify

--- src/services/FirestoreService.js
+++ src/services/FirestoreService.js
@@ -2,10 +2,10 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getFirebaseConfig } from './firebase-config';
 
-const firebaseConfig = getFirebaseConfig();
+class FirestoreService {
+  init = (app) => {
+    this.firestore = getFirestore(app);
+  };
+}
 
-const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
-
-export {};
+export default new FirestoreService();
