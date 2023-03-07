3bf524748fc6d60d0db64bb0e3e6c3ca3fdeafab
gregorsunta <gresunta@gmail.com>
2023-03-07 22:13:23 +0100

    fix error

--- src/App.js
+++ src/App.js
@@ -5,17 +5,15 @@ import { observer } from 'mobx-react';
 import { getAuth, GoogleAuthProvider, initializeAuth } from 'firebase/auth';
 import { RouterConfig } from './navigation';
 import authStore from './stores/authStore';
+import FirestoneService from './services/FirestoreService';
 import { getFirebaseConfig } from './services/firebase-config';
 import './App.css';
 import { AuthContext } from './context/authContext';
 
 const app = initializeApp(getFirebaseConfig());
-
 const auth = getAuth(app);
-
 const store = new authStore(auth);
-
-const db = getFirestore();
+FirestoneService.init(app);
 
 const App = () => {
   return (
