7eaecd9a2a7dd6c0ef95c91814cd07cef795bbf0
gregorsunta <gresunta@gmail.com>
2023-03-08 12:57:46 +0100

    Modify to make it more reusable

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -3,11 +3,19 @@ import styles from '../../styles/atoms/Button.module.css';
 const Button = ({
   text = null,
   onClick = null,
+  isActive = false,
   className = '',
+  activeClassName = '',
   title = undefined,
+  key = null,
 }) => {
   return (
-    <button title={title} className={`${''} ${className}`} onClick={onClick}>
+    <button
+      title={title}
+      className={`${className} ${isActive ? activeClassName : ''}`}
+      onClick={onClick}
+      key={key}
+    >
       {text}
     </button>
   );
