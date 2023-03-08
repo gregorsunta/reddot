89eab7f25091ece5a6e9bd57273f38a23254052d
gregorsunta <gresunta@gmail.com>
2023-03-08 12:57:46 +0100

    Revert "Modify to make it more reusable"
    This reverts commit 7eaecd9a2a7dd6c0ef95c91814cd07cef795bbf0.

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -3,19 +3,11 @@ import styles from '../../styles/atoms/Button.module.css';
 const Button = ({
   text = null,
   onClick = null,
-  isActive = false,
   className = '',
-  activeClassName = '',
   title = undefined,
-  key = null,
 }) => {
   return (
-    <button
-      title={title}
-      className={`${className} ${isActive ? activeClassName : ''}`}
-      onClick={onClick}
-      key={key}
-    >
+    <button title={title} className={`${''} ${className}`} onClick={onClick}>
       {text}
     </button>
   );
