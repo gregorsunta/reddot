1b62317a3bfbe36fd6f065f6e7da93c15c28f56c
gregorsunta <gresunta@gmail.com>
2023-03-09 14:59:54 +0100

    Make more reusable

--- src/components/molecules/ButtonsGroup.jsx
+++ src/components/molecules/ButtonsGroup.jsx
@@ -1,18 +1,11 @@
 import { Button } from '../atoms';
-import { withRedirect } from '../../utils/withRedirect.jsx';
 import { observer } from 'mobx-react';
-
-const RedirectButton = withRedirect(Button);
 
 const ButtonsGroup = observer(({ buttons }) => {
   return (
     <>
-      {buttons.map(({ link, ...rest }) => {
-        return link ? (
-          <RedirectButton link={link} {...rest} />
-        ) : (
-          <Button {...rest} />
-        );
+      {buttons.map(({ ...rest }) => {
+        return <Button {...rest} />;
       })}
     </>
   );
