7c88d3f925b9c451359ee183637e2876a2b9dfc4
gregorsunta <gresunta@gmail.com>
2023-03-13 19:33:07 +0100

    Setup store

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,7 +1,16 @@
+import { action, makeObservable, observable } from 'mobx';
+
 export class UserStore {
+  user = null;
   constructor(RootStore) {
     this.RootStore = RootStore;
+    makeObservable(this, {
+      user: observable,
+      setUser: action,
+    });
   }
+  setUser = (user) => {
+    this.user = user;
+  };
 }
-
 export default new UserStore();
