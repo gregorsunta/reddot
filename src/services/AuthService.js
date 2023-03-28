e7f96eaeb061cd433c593c139ecc5b7fb35c9bda
gregorsunta <gresunta@gmail.com>
2023-03-28 12:11:49 +0200

    Rename function and fix export (eslint)

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -6,7 +6,7 @@ import {
 } from 'firebase/auth';
 
 class AuthService {
-  signInWithGoogle = async () => {
+  signInWithGooglePopup = async () => {
     const provider = new GoogleAuthProvider();
     try {
       return signInWithPopup(getAuth(), provider);
@@ -23,4 +23,5 @@ class AuthService {
   };
 }
 
-export default new AuthService();
+const authService = new AuthService();
+export default authService;
