377d30f7457af3f4e3a682d385c9de709b40771d
gregorsunta <gresunta@gmail.com>
2023-03-28 15:44:42 +0200

    Move buttons array, add functionality with .map
    - Move buttons array outside component
    - Add state changing callback functions to the buttons with mapping

--- src/components/molecules/panels/SubmitPanel.jsx
+++ src/components/molecules/panels/SubmitPanel.jsx
@@ -12,6 +12,51 @@ import styles from '../../../styles/molecules/panels/SubmitPanel.module.css';
 import { Button } from '../../atoms/Button';
 import { ButtonsGroup } from '../';
 
+const buttons = [
+  {
+    variant: 'outlined',
+    children: 'Text',
+    startIcon: <TextOutline />,
+    type: 'text',
+    className: styles.btn,
+    activeClassName: styles.active,
+  },
+  {
+    variant: 'outlined',
+    children: 'Image & Video',
+    startIcon: <ImageOutline />,
+    type: 'visual',
+    className: styles.btn,
+    activeClassName: styles.active,
+  },
+  {
+    variant: 'outlined',
+    children: 'Link',
+    startIcon: <LinkOutline />,
+    type: 'link',
+    className: styles.btn,
+    activeClassName: styles.active,
+  },
+  {
+    variant: 'outlined',
+    children: 'Poll',
+    startIcon: <AlbumsOutline />,
+    type: 'poll',
+    className: styles.btn,
+    activeClassName: styles.active,
+    disabled: true,
+  },
+  {
+    variant: 'outlined',
+    children: 'Live',
+    startIcon: <MicOutline />,
+    type: 'live',
+    className: styles.btn,
+    activeClassName: styles.active,
+    disabled: true,
+  },
+];
+
 const SubmitPanel = () => {
   const [activeType, setActiveType] = useState('text');
   const isActive = (type) => {
@@ -44,63 +89,17 @@ const SubmitPanel = () => {
         console.error(`The entered panel type is invalid. Type: ${activeType}`);
     }
   };
-  const buttons = [
-    {
-      variant: 'outlined',
-      children: 'Text',
-      startIcon: <TextOutline />,
-      type: 'text',
-      className: styles.btn,
-      activeClassName: styles.active,
-      onClick: changeType.bind(this, 'text'),
-      isActive: activeType === 'text',
-    },
-    {
-      variant: 'outlined',
-      children: 'Image & Video',
-      startIcon: <ImageOutline />,
-      type: 'visual',
-      className: styles.btn,
-      activeClassName: styles.active,
-      onClick: changeType.bind(this, 'visual'),
-      isActive: activeType === 'visual',
-    },
-    {
-      variant: 'outlined',
-      children: 'Link',
-      startIcon: <LinkOutline />,
-      type: 'link',
-      className: styles.btn,
-      activeClassName: styles.active,
-      onClick: changeType.bind(this, 'link'),
-      isActive: activeType === 'link',
-    },
-    {
-      variant: 'outlined',
-      children: 'Poll',
-      startIcon: <AlbumsOutline />,
-      type: 'poll',
-      className: styles.btn,
-      activeClassName: styles.active,
-      onClick: changeType.bind(this, 'poll'),
-      isActive: activeType === 'poll',
-      disabled: true,
-    },
-    {
-      variant: 'outlined',
-      children: 'Live',
-      startIcon: <MicOutline />,
-      type: 'live',
-      className: styles.btn,
-      activeClassName: styles.active,
-      onClick: changeType.bind(this, 'live'),
-      isActive: activeType === 'live',
-      disabled: true,
-    },
-  ];
+
   return (
     <div className={`${`${indexStyles.container}`} ${styles.container}`}>
-      <ButtonsGroup className={styles['btns-container']} buttons={buttons} />
+      <ButtonsGroup
+        className={styles['btns-container']}
+        buttons={buttons.map((btn) => ({
+          ...btn,
+          onClick: changeType.bind(btn, btn.type),
+          isActive: isActive(btn.type),
+        }))}
+      />
       <input className={`${styles.input}`} type="text" placeholder="Title" />
       {displayType()}
       <div className={styles['form-btn']}>
