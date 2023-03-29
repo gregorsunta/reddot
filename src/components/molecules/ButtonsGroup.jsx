312e04059b10758e5c665258f06b7d789e87a118
gregorsunta <gresunta@gmail.com>
2023-03-29 13:26:54 +0200

    Re-design component
    - Make component more readable with prop grouping to expectedStyles and ownerStyles (custom styles)
    - Make component less error prone with prop type checking
    - Make component more reusable with removing button creation (the buttons are passed now as components, not objects)

--- src/components/molecules/ButtonsGroup.jsx
+++ src/components/molecules/ButtonsGroup.jsx
@@ -1,14 +1,26 @@
-import { Button } from '../atoms';
+import PropTypes from 'prop-types';
 import { observer } from 'mobx-react';
+import { Button } from '../atoms';
 
-const ButtonsGroup = observer(({ buttons, className }) => {
-  return (
-    <div className={className}>
-      {buttons.map(({ ...rest }) => {
-        return <Button {...rest} />;
-      })}
-    </div>
-  );
-});
+const createExpectedClasses = (expectedProps) => {
+  return [
+    `flex-${expectedProps.orientation}`,
+    `variant-${expectedProps.variant}`,
+  ].join(' ');
+};
+const ButtonsGroup = observer(
+  ({ children, ownerClasses, orientation, variant }) => {
+    const expectedProps = { orientation, variant };
+    const expectedClasses = createExpectedClasses(expectedProps);
 
+    return (
+      <div className={`${expectedClasses} ${ownerClasses}`}>{children}</div>
+    );
+  },
+);
+ButtonsGroup.propTypes = {
+  ownerClasses: PropTypes.array,
+  orientation: PropTypes.oneOf(['horizontal', 'vertical']),
+  variant: PropTypes.oneOf(['outlined', 'solid', 'text', 'icon']),
+};
 export { ButtonsGroup };
