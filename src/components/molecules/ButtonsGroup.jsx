c017035588c9811ff15b80573b4667c36b78a118
gregorsunta <gresunta@gmail.com>
2023-03-29 21:32:03 +0200

    Change semantics and add default styles
    Change div to a ul and map through children to make li element for better semantic meaning
    Add default styles

--- src/components/molecules/ButtonsGroup.jsx
+++ src/components/molecules/ButtonsGroup.jsx
@@ -1,19 +1,28 @@
 import PropTypes from 'prop-types';
+import styles from '../../styles/molecules/ButtonsGroup.module.css';
 
 const createExpectedClasses = (expectedProps) => {
   return [
     `flex-${expectedProps.orientation}`,
     `variant-${expectedProps.variant}`,
+    styles.container,
   ].join(' ');
 };
 const ButtonsGroup = ({ children, ownerClasses, orientation, variant }) => {
   const expectedProps = { orientation, variant };
   const expectedClasses = createExpectedClasses(expectedProps);
 
-  return <div className={`${expectedClasses} ${ownerClasses}`}>{children}</div>;
+  return (
+    <ul className={`${expectedClasses} ${ownerClasses}`}>
+      {children.map((item) => (
+        <li key={item.key}>{item}</li>
+      ))}
+    </ul>
+  );
 };
 
 ButtonsGroup.propTypes = {
+  children: PropTypes.array, //only two ore more items are allowed
   ownerClasses: PropTypes.array,
   orientation: PropTypes.oneOf(['horizontal', 'vertical']),
   variant: PropTypes.oneOf(['outlined', 'solid', 'text', 'icon']),
