f662c113240c955c1ebed89d6b0874327cbfa9eb
gregorsunta <gresunta@gmail.com>
2023-04-02 10:11:37 +0200

    Add get firestore posts funtionality and display
    Use an initial query to get posts from firestore

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { useFirestoreService } from '../../context/firestoreServiceContext';
 import { CreatePostPanel, PageInfoPanel, FeedPanel } from '../molecules/panels';
 import MainTemplate from '../templates/MainTemplate';
 
 const HomePage = () => {
+  const firestoreService = useFirestoreService();
+  const [posts, setPosts] = useState(null);
+  const getPosts = async () => {
+    const posts = await firestoreService.getPost();
+    setPosts(posts);
+  };
+  useEffect(() => {
+    getPosts();
+  }, []);
   return (
     <MainTemplate
       content={
         <>
           <CreatePostPanel />
-          <FeedPanel />
+          {posts?.map((post) => (
+            <FeedPanel post={post}></FeedPanel>
+          ))}
         </>
       }
       side={<PageInfoPanel />}
