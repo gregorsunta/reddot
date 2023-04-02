0d8991486a053c566fb3e0fb207b42ecce2255d9
gregorsunta <gresunta@gmail.com>
2023-04-02 10:11:03 +0200

    Add firestore provider context

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './utils/utilityStyles.css';
 import React, { useEffect, useState } from 'react';
 import { initializeApp } from 'firebase/app';
 import { IconContext } from 'react-icons';
-import FirestoneService from './services/FirestoreService';
+import { firestoreService } from './services/FirestoreService';
 import { getFirebaseConfig } from './services/firebase-config';
 import AuthStore from './stores/authStore';
 import { AuthStoreContext } from './context/authStoreContext';
+import { FirestoreServiceContext } from './context/firestoreServiceContext';
+
 import { RouterConfig } from './navigation';
 
 const app = initializeApp(getFirebaseConfig());
-FirestoneService.init(app);
+firestoreService.init(app);
 
 const App = () => {
   const [authStore] = useState(() => AuthStore);
@@ -20,11 +22,13 @@ const App = () => {
   }, [authStore]);
 
   return (
-    <IconContext.Provider value={{ size: '100%' }}>
+    <FirestoreServiceContext.Provider value={firestoreService}>
       <AuthStoreContext.Provider value={authStore}>
-        <RouterConfig />
+        <IconContext.Provider value={{ size: '100%' }}>
+          <RouterConfig />
+        </IconContext.Provider>
       </AuthStoreContext.Provider>
-    </IconContext.Provider>
+    </FirestoreServiceContext.Provider>
   );
 };
 
