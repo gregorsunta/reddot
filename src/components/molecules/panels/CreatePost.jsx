5dfa4ceed3e53556493114f4d791bbb20542bdfb
gregorsunta <gresunta@gmail.com>
2023-04-17 12:39:55 +0200

    Add content and styles

--- src/components/molecules/panels/CreatePost.jsx
+++ src/components/molecules/panels/CreatePost.jsx
@@ -1,11 +1,17 @@
 import { Link } from 'react-router-dom';
 import styles from '../../../styles/molecules/panels/CreatePostPanel.module.css';
 import { Input } from '../../atoms';
+import { useAuthStore } from '../../../context/authStoreContext';
 
 const CreatePost = () => {
+  const authStore = useAuthStore();
   return (
     <div className={`panel ${styles.container}`}>
-      <p>PI</p>
+      <img
+        className={styles['profile-icon']}
+        src={authStore?.user?.photoURL}
+        alt="User profile icon"
+      />
       <Link to="/submit">
         <Input
           className={`${styles.input}`}
