ae443663fcf6a0994dcd76a2ffd62eb578653309
gregorsunta <gresunta@gmail.com>
2023-04-17 11:18:45 +0200

    Add missing export

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,6 +1,7 @@
 export * from './ButtonGroup.jsx';
 export * from './Dropdown.jsx';
 export * from './Modal.jsx';
+export * from './Stack.jsx';
 export * from './Logo.jsx';
 export * from './panels/BriefPostPanel.jsx';
 export * from './panels/PostPanel.jsx';
