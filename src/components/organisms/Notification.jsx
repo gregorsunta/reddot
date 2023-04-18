7985b77bfc95c820d36c27d1b4b22ad1049d1718
gregorsunta <gresunta@gmail.com>
2023-04-18 13:53:05 +0200

    Add styles

--- src/components/organisms/Notification.jsx
+++ src/components/organisms/Notification.jsx
@@ -1,9 +1,15 @@
-const Notification = () => {
-  return (
-    <div>
-      <p></p>
-    </div>
-  );
+import { createUseStyles } from 'react-jss';
+import { SIZES } from '../../constants';
+
+const useStyles = createUseStyles({
+  container: {
+    padding: `${SIZES.SIZE_64}`,
+  },
+});
+
+const Notification = ({ children }) => {
+  const styles = useStyles();
+  return <div className={styles.container}>{children}</div>;
 };
 
-export default Notification;
+export { Notification };
