b76c8a7dda0a0a19daef0bd95d0e03ac0a2a2394
gregorsunta <gresunta@gmail.com>
2023-04-19 10:36:56 +0200

    Implment JSS styles and delete redundant files

--- src/components/molecules/Panel.jsx
+++ src/components/molecules/Panel.jsx
@@ -1,4 +1,15 @@
 import styles from '../../styles/molecules/panels/Panel.module.css';
+import { createUseStyles } from 'react-jss';
+import { SIZES, NEUTRAL } from '../../constants/StyleConstants';
+
+const useStyles = createUseStyles({
+  container: () => ({
+    padding: `${SIZES.SIZE_1}`,
+    borderRadius: `var(--size-2)`,
+    border: `${SIZES.SIZE_1} solid hsl(${NEUTRAL.HS},${NEUTRAL.L_90} )`,
+    backgroundColor: `white`,
+  }),
+});
 
 const Panel = ({ children }) => {
   return <div className={styles.container}>{children}</div>;
--- src/components/templates/MainTemplate.jsx
+++ src/components/templates/MainTemplate.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
-import styles from '../../styles/templates/MainTemplate.module.css';
+import { createUseStyles } from 'react-jss';
+import { Stack } from '../molecules/';
+import { LIGHTNESS, NEUTRAL, SIZES } from '../../constants';
+
+const useContainerStyles = createUseStyles({
+  container: {
+    maxWidth: '1100px',
+    margin: `0 auto`,
+    padding: `${SIZES.SIZE_56}`,
+  },
+  background: {
+    backgroundColor: `hsl(${NEUTRAL.HS} ${LIGHTNESS.L_90})`,
+  },
+});
+const useContentStyles = createUseStyles({
+  content: {
+    flex: 4,
+  },
+});
+const useSideStyles = createUseStyles({
+  side: {
+    flex: 2,
+  },
+});
 
 const MainTemplate = ({ auth, content, side }) => {
+  const containerClassNames = useContainerStyles();
+  const contentClassNames = useContentStyles();
+  const sideClassNames = useSideStyles();
+
   return (
-    <div className={styles['main-background']}>
-      <div className={styles['main-wrapper']}>
-        <div className={styles.main}>
-          <section className={styles.content}>{content}</section>
-          <section className={styles['side-panel-container']}>{side}</section>
-        </div>
-      </div>
+    <div className={containerClassNames.background}>
+      <Stack
+        orientation="row"
+        spacing={SIZES.SIZE_16}
+        customClassName={containerClassNames.container}
+      >
+        <section className={contentClassNames.content}>{content}</section>
+        <section className={sideClassNames.side}>{side}</section>
+      </Stack>
     </div>
   );
 };
