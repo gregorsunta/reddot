fdb95d88d1b957ad044e3035384286daf7887015
gregorsunta <gresunta@gmail.com>
2023-06-03 14:29:17 +0200

    refactor code

--- src/utils/withRedirect.jsx
+++ src/utils/withRedirect.jsx
@@ -1,14 +1,12 @@
 import { Link } from 'react-router-dom';
 
 const withRedirect = (Component) => {
-  const WithRedirect = ({ link, ...rest }) => {
-    return (
-      <Link to={link}>
-        <Component {...rest}></Component>
-      </Link>
-    );
-  };
-  return WithRedirect;
+  const { link, ...rest } = Component;
+  return (
+    <Link to={link}>
+      <Component {...rest}></Component>
+    </Link>
+  );
 };
 
 export { withRedirect };
